513c5f816f0d093a10b96d0ca1ebf979415b5cc3
anshu aarogyahub <anshu@aarogyahub.com>
2021-10-05 15:39:46 +0530

    chakra ui installed

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+
+function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: React.ComponentType;
+  pageProps: any;
+}) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
